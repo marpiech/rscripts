c2ee6a5a3561a5eb67d7f580f1b1d9e4c91bd0e2
Marcin Piechota <piechota.marcin@gmail.com>
2017-12-21 10:39:53 +0100

    Create marpiech-rscripts.R

---
+++ marpiech-rscripts.R
@@ -0,0 +1,8 @@
+load_package <- function(package.name) {
+  if (!require(package.name, character.only = TRUE)) {
+    install.packages(package.name, dep=TRUE)
+    if(!require(package.name, character.only = TRUE)) stop("Package not found")
+  }
+}
+
+source("https://raw.githubusercontent.com/marpiech/rscripts/master/enrichR.R")
