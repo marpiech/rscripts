cc8bcaafc22504da1cb0f25b63d581235e298166
Marcin Piechota <piechota.marcin@gmail.com>
2018-02-26 14:44:03 +0100

    Update enrichR.R

--- enrichR.R
+++ enrichR.R
@@ -12,3 +12,40 @@ get_pubmed_title <- function(pubmed.id){
     return("")
   }
 }
+
+get_enrichr <- function(genes, database, threshold = 0.05, maxn = 25, ngenes.threshold = 2) {
+  
+  enrichr_data = enrichr(genes, databases = database)
+
+for(term in names(enrichr_data)){
+  terms = c(head(enrichr_data[[term]]$Term, n = maxn))
+  ppals = c(head(enrichr_data[[term]]$P.value, n = maxn))
+  pvals = c(head(enrichr_data[[term]]$Adjusted.P.value, n = maxn))
+  genes = c(head(enrichr_data[[term]]$Genes, n = maxn))
+  terms = terms[pvals < threshold]
+  ppals = ppals[pvals < threshold]
+  genes = genes[pvals < threshold]
+  pvals = pvals[pvals < threshold]
+  
+  require(stringr)
+  ngenes = sapply(genes, function(x) {length(str_count(x, ";")) + 1})
+  terms = terms[ngenes >= ngens.threshold]
+  ppals = ppals[ngenes >= ngens.threshold]
+  genes = genes[ngenes >= ngens.threshold]
+  pvals = pvals[ngenes >= ngens.threshold]
+  
+  
+  if (term == "GeneSigDB"){
+    terms =  get_pubmed_title(c(sapply(strsplit(terms, "-"), head, 1)))
+  }
+  
+  if (length(pvals) == 0){
+    terms = c("BRAK")
+    genes = c("BRAK")
+    pvals = c("BRAK")
+    ppals = c("BRAK")
+  }
+  return((cbind(terms, genes, pvals, ppals)))
+  
+  }
+}
